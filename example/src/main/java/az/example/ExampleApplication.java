f0c1ff029d71f1d1a0c7fe75d82340664c9ccb82
Rovshan Aghayev <rovsenaqazadeh2@gmail.com>
2026-04-26 19:24:32 +0400

    Add example module with Spring Boot configuration and event dispatcher integration

---
+++ example/src/main/java/az/example/ExampleApplication.java
@@ -0,0 +1,11 @@
+package az.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+    static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+}
