0ab221f8745795c67155e43f8706f38e46849bbf
Rovshan Aghayev <rovsenaqazadeh2@gmail.com>
2026-05-31 12:41:24 +0400

    Refactor variable declarations in ProductCategoryModerationDomainServiceImpl for consistency and clarity

--- catalog-management/catalog-command-service/catalog-command-domain/catalog-command-domain-core/src/main/java/az/kon/academy/catalog/command/service/domain/core/port/inbound/service/category/ProductCategoryModerationDomainServiceImpl.java
+++ catalog-management/catalog-command-service/catalog-command-domain/catalog-command-domain-core/src/main/java/az/kon/academy/catalog/command/service/domain/core/port/inbound/service/category/ProductCategoryModerationDomainServiceImpl.java
@@ -20,56 +20,56 @@ public final class ProductCategoryModerationDomainServiceImpl implements Product
 
     @Override
     public ProductCategoryRoot changeInformation(SeDomainContext context, ProductCategoryChangeInformationCommand command) {
-        var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
+        final var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
         return category.changeInformation(command);
     }
 
     @Override
     public ProductCategoryRoot changeImage(SeDomainContext context, ProductCategoryChangeImageCommand command) {
-        var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
+        final var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
         return category.changeImage(command);
     }
 
     @Override
     public ProductCategoryRoot changeParent(SeDomainContext context, ProductCategoryChangeParentCommand command) {
-        var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
+        final var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
         if (Objects.isNull(command.getParentId()))
             return category.removeParent();
 
-        var parentCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var parentCategory = parentCategoryQueryPort.fetchById(command.getParentId());
+        final var parentCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var parentCategory = parentCategoryQueryPort.fetchById(command.getParentId());
         return category.changeParent(parentCategory.getRootID());
     }
 
     @Override
     public ProductCategoryRoot archive(SeDomainContext context, ProductCategoryArchiveCommand command) {
-        var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
+        final var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
         return category.archive();
     }
 
     @Override
     public ProductCategoryRoot activate(SeDomainContext context, ProductCategoryActivateCommand command) {
-        var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
+        final var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
         return category.activate();
     }
 
     @Override
     public ProductCategoryRoot delete(SeDomainContext context, ProductCategoryDeleteCommand command) {
-        var productQueryPort = context.getQueryPort(ProductQueryOutboundPort.class);
-        var existsProduct = productQueryPort.exitsByCategoryId(command.getProductCategoryId());
+        final var productQueryPort = context.getQueryPort(ProductQueryOutboundPort.class);
+        final var existsProduct = productQueryPort.exitsByCategoryId(command.getProductCategoryId());
         if(existsProduct) {
             throw new ProductCategoryDomainException(
                     ProductCategoryDomainErrorCodes.HAS_ACTIVE_PRODUCT,
                     List.of(command.getProductCategoryId().toString())
             );
         }
-        var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
-        var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
+        final var productCategoryQueryPort = context.getQueryPort(ProductCategoryQueryOutboundPort.class);
+        final var category = productCategoryQueryPort.fetchById(command.getProductCategoryId());
         return category.delete();
     }
 }
